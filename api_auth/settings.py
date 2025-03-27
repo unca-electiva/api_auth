df3f55242c684739fc5d3e6620084b7e287aac34
ceciga <cecigallardo@gmail.com>
2025-03-27 18:32:02 -0300

    Configuracion jwt

--- api_auth/settings.py
+++ api_auth/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'autenticacion',
 ]
 
@@ -110,6 +111,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',  # Requiere autenticación
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
 }
 
 # Internationalization
