f68691c7444adf8e23037ea871b5c41e366a5149
ceciga <cecigallardo@gmail.com>
2025-04-25 20:59:28 -0300

    configuraciones

--- autenticacion/api.py
+++ autenticacion/api.py
@@ -9,8 +9,7 @@ from autenticacion.serializers import AnuncioSerializer
 class AnuncioViewSet(viewsets.ModelViewSet):
     queryset = Anuncio.objects.all()
     serializer_class = AnuncioSerializer
-    permission_classes = [IsAuthenticated, TokenHasReadWriteScope]
+    permission_classes = [TokenHasReadWriteScope]
 
     def perform_create(self, serializer):
-        usuario = Usuario.objects.get(id=1)
-        serializer.save(publicado_por=usuario)
+        serializer.save(publicado_por=self.request.user)
