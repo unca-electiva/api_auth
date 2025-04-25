8f738882b1299dbe508beef5983320fbfc65f573
ceciga <cecigallardo@gmail.com>
2025-04-25 19:44:36 -0300

    Cambio en la vista

--- autenticacion/api.py
+++ autenticacion/api.py
@@ -8,8 +8,6 @@ from autenticacion.serializers import AnuncioSerializer
 class AnuncioViewSet(viewsets.ModelViewSet):
     queryset = Anuncio.objects.all()
     serializer_class = AnuncioSerializer
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
 
     def perform_create(self, serializer):
-        usuario = Usuario.objects.get(id=1)
-        serializer.save(publicado_por=usuario)
+        serializer.save(publicado_por=self.request.user)
